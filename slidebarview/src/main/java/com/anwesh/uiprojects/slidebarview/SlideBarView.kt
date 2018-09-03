a5310a0fe25d6cc943a4dd06f139e29de079ff0b
Anwesh43 <anweshthecool0@gmail.com>
2018-09-03 10:55:21 +0530

    adding SlideBarView class

--- slidebarview/src/main/java/com/anwesh/uiprojects/slidebarview/SlideBarView.kt
+++ slidebarview/src/main/java/com/anwesh/uiprojects/slidebarview/SlideBarView.kt
@@ -31,3 +31,21 @@ fun Canvas.drawSlideBarNode(i : Int, scale : Float, currI : Int, paint : Paint) 
     }
     restore()
 }
+
+class SlideBarView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
