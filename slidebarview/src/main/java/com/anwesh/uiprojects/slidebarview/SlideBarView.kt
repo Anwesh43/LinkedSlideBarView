4eeb8727b2337f70b8cf79208e3c1adbdd6bc498
Anwesh43 <anweshthecool0@gmail.com>
2018-09-03 10:48:09 +0530

    adding slidebarview module

--- slidebarview/src/main/java/com/anwesh/uiprojects/slidebarview/SlideBarView.kt
+++ slidebarview/src/main/java/com/anwesh/uiprojects/slidebarview/SlideBarView.kt
@@ -3,3 +3,11 @@ package com.anwesh.uiprojects.slidebarview
 /**
  * Created by anweshmishra on 03/09/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.content.Context
+
+val nodes : Int = 5
