c2f9df7c41ae766795c2a2db2b83f2d6a8a65617
Anwesh43 <anweshthecool0@gmail.com>
2018-09-03 10:53:41 +0530

    adding drawSlideBarNode canvas extension method

--- slidebarview/src/main/java/com/anwesh/uiprojects/slidebarview/SlideBarView.kt
+++ slidebarview/src/main/java/com/anwesh/uiprojects/slidebarview/SlideBarView.kt
@@ -9,5 +9,25 @@ import android.view.MotionEvent
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.content.Context
+import android.graphics.Color
 
 val nodes : Int = 5
+
+fun Canvas.drawSlideBarNode(i : Int, scale : Float, currI : Int, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = (w * 0.9f) / nodes
+    paint.strokeWidth = Math.min(w, h) / 60
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(0.05f * w + gap * i, h/2)
+    paint.color = Color.parseColor("#616161")
+    drawLine(0f, 0f, gap, 0f, paint)
+    paint.color = Color.parseColor("#1976D2")
+    drawLine(0f, 0f, gap * scale, 0f, paint)
+    if (i == currI) {
+        paint.color = Color.parseColor("#FFD600")
+        drawCircle(gap * scale, 0f, gap/10, paint)
+    }
+    restore()
+}
